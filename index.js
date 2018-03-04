5b0a1f21bb797fc07276f35018a758de4dd47e42
caillet <jp78920@hotmail.com>
2018-03-04 22:00:35 +0100

    refactor(eslint format and format for package)

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const request = require('request');
-const https = require("https");
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const https = require('https');
 
-
-class Botyt {
-  constructor(requete) {
+module.exports = class Botyt {
+  constructor (requete) {
     this.requete = requete;
   }
 
@@ -12,51 +9,48 @@ class Botyt {
    * Initialize
    * @return {Error | string} result
    */
-  init() {
-  	 var sync = true;
-    
-    const url ="https://www.googleapis.com/youtube/v3/search?part=snippet&q="+this.requete+"&key=AIzaSyDzC9gAicvS0LVilYaM-9VtMSfMnucQY90";
-	https.get(url, (resp) => {
-  		this.data = ''; 
-  		// A chunk of data has been recieved.
-  		resp.on('data', (chunk) => {
-    		this.data += chunk;
-  		});
-  		// The whole response has been received. Print out the result.
-  		resp.on('end', () => {
-  			this.setJson(JSON.parse(this.data));
-    		sync =false;
-		});
- 
-	}).on("error", (err) => {
-  		console.log("Error: " + err.message);
-	});
-	while(sync) {require('deasync').sleep(100);}
-	//console.log(this.json);
-	}
+  init () {
+    var sync = true;
+    const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + this.requete + '&key=AIzaSyDzC9gAicvS0LVilYaM-9VtMSfMnucQY90';
 
-  setJson(json){
-    this.json = json;
-	}
-
-  getJson(){
-	return this.json;
+    https.get(url, (resp) => {
+      this.data = '';
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        this.data += chunk;
+      });
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        this.setJson(JSON.parse(this.data));
+        sync = false;
+      });
+    }).on('error', (err) => {
+      console.log('Error: ' + err.message);
+    });
+    while (sync) {
+      require('deasync').sleep(100);
+    }
+    //console.log(this.json);
   }
 
-  getId(){
-  	return this.json.items[0].id.videoId;
+  setJson (json) {
+    this.json = json;
+  }
+
+  getJson () {
+    return this.json;
+  }
+
+  getId () {
+    return this.json.items[0].id.videoId;
   }
 
   /**
    * Run
    * @return {Error | string} result
    */
-  run() {
-  	this.init();    
+  run () {
+    this.init();
   }
-
-  
 };
-
-
 
